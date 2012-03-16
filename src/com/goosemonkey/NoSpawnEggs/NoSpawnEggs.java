0bbc8e875fd497fbd8966e04a423dce5367533de
Joshua Prince <goosemonkey97@gmail.com>
2012-03-15 20:36:33 -0700

    #26: Added Vault support for perms

--- src/com/goosemonkey/NoSpawnEggs/NoSpawnEggs.java
+++ src/com/goosemonkey/NoSpawnEggs/NoSpawnEggs.java
@@ -1,10 +1,13 @@
 package com.goosemonkey.NoSpawnEggs;
 
+import net.milkbowl.vault.permission.Permission;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.goosemonkey.NoSpawnEggs.NewConfig.*;
@@ -14,6 +17,9 @@ public class NoSpawnEggs extends JavaPlugin
 	//Config files:
 	private static Main mainConfig;
 	private static Locale localeConfig;
+	
+    private static Permission perms = null;
+    private static boolean vault = false;
 	
 	public void onEnable() 
 	{		
@@ -28,6 +34,14 @@ public class NoSpawnEggs extends JavaPlugin
 		this.getServer().getPluginManager().registerEvents(new DispenseListener(), this);
 		this.getServer().getPluginManager().registerEvents(new XPBottleListener(), this);
 		this.getServer().getPluginManager().registerEvents(new EnderEyeListener(), this);
+		
+		//Permissions
+		if (this.setupPermissions())
+		{
+			this.getLogger().info("Hooked with Vault successfully!");
+			
+			NoSpawnEggs.vault = true;
+		}
 		
 		//Ready!
 		this.getLogger().info("NoSpawnEggs v"+this.getDescription().getVersion()+" enabled!");
@@ -101,7 +115,14 @@ public class NoSpawnEggs extends JavaPlugin
 	 */
 	public static boolean hasPermission(Player player, String perm)
 	{
-		return player.hasPermission(perm);
+		if (NoSpawnEggs.vault)
+		{
+			return NoSpawnEggs.perms.has(player, perm);
+		}
+		else
+		{
+			return player.hasPermission(perm);
+		}
 	}
 	
 	/**
@@ -112,6 +133,30 @@ public class NoSpawnEggs extends JavaPlugin
 	 */
 	public static boolean hasPermission(CommandSender sender, String perm)
 	{
-		return sender.hasPermission(perm);
+		if (NoSpawnEggs.vault)
+		{
+			return NoSpawnEggs.perms.has(sender, perm);
+		}
+		else
+		{
+			return sender.hasPermission(perm);
+		}
 	}
+	
+	private boolean setupPermissions()
+	{
+		try
+		{
+			RegisteredServiceProvider<Permission> rsp = 
+					getServer().getServicesManager().getRegistration(Permission.class);
+			
+			NoSpawnEggs.perms = rsp.getProvider();
+		}
+		catch (NoClassDefFoundError ex)
+		{
+			return false;
+		}
+		
+		return perms != null;
+    }
 }
